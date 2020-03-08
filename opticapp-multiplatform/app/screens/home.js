7974bff7eb31768ae869c43ef16cff1d5989588f
softmild2106 <57544387+softmild2106@users.noreply.github.com>
2020-03-08 15:26:57 -0500

    add button to home page, splash and image background

--- opticapp-multiplatform/app/screens/home.js
+++ opticapp-multiplatform/app/screens/home.js
@@ -1,17 +1,38 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet, ImageBackground } from "react-native";
 
 export default function Home({ navigation }) {
   return (
-    <View>
-      <Button
-        title="Go to Inventario"
-        onPress={() => navigation.navigate("Inventario", { tipo: "sol" })}
-      />
-      <Button
-        title="Go to Catalogo"
-        onPress={() => navigation.navigate("Catalog")}
-      />
-    </View>
+    <ImageBackground
+      style={{ flex: 1 }}
+      //We are using online image to set background
+      source={require("../../assets/drawable-xxxhdpi/mainactimg.png")}
+    >
+      <View style={styles.buttonCatalogo}>
+        <Button
+          title="Go to Catalogo"
+          onPress={() => navigation.navigate("Catalog")}
+        />
+      </View>
+      <View style={styles.buttonFormula}>
+        <Button
+          title="Formula"
+          onPress={() => navigation.navigate("Inventario", { tipo: "sol" })}
+        />
+      </View>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonFormula: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: 80
+  },
+  buttonCatalogo: {
+    flex: 5,
+    justifyContent: "flex-end",
+    marginBottom: 0
+  }
+});
